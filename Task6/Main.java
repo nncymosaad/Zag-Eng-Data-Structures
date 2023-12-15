c22b1ea84a749ec8de7e7ce0c28bd4b5681a237f
nancy <125913682+nncymosaad@users.noreply.github.com>
2023-12-15 21:52:10 +0200

    Update Main.java

--- Task6/Main.java
+++ Task6/Main.java
@@ -5,12 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String l = "Nancy";
         System.out.println(reverseString(l));
-        String M = "[{lkop}]";
-        System.out.println(balancedExpression(M));
-        String N = "[{lkop}])";
-        System.out.println(balancedExpression(N));
-        String Z = "([{lkop}]";
-        System.out.println(balancedExpression(Z));
+
     }
     public static String reverseString(String s) {
         Stack<Character> stack = new Stack<>();
@@ -22,33 +17,3 @@ public class Main {
             reverse  += stack.pop();
         return reverse;
     }
-    public static Boolean balancedExpression(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if(isLeftBracket(s.charAt(i))) {
-                stack.push(s.charAt(i));
-            } else if (isRightBracket(s.charAt(i))) {
-                if(stack.isEmpty())
-                    return false;
-                char left = stack.pop();
-                if(!isMatch(left,s.charAt(i)))
-                    return false;
-            }
-        }
-        return stack.isEmpty();
-    }
-
-    private static Boolean isMatch(char left, char right) {
-        return (left=='['&&right==']')||
-               (left=='{'&&right=='}')||
-               (left=='('&&right==')')||
-               (left=='<'&&right=='>');
-    }
-
-    private static boolean isLeftBracket(char c) {
-        return c=='[' || c=='{' || c=='(' || c=='<' ;
-    }
-    private static boolean isRightBracket(char c) {
-        return c==']' || c=='}' || c==')' || c=='>' ;
-    }
-}
